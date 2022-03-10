ae670753445070149310b2036a35b0c1239750de
bartosz.wilczak <bartosz.wilczak@PK5-15>
2022-03-10 13:09:53 +0100

    ver2

--- lab_01/lab_01/Program.cs
+++ lab_01/lab_01/Program.cs
@@ -9,4 +9,49 @@ namespace lab_01
             Console.WriteLine("Hello World!");
         }
     }
+    public class Ulamek
+    {
+        private int licznik;
+        public int Licznik
+        {
+            get
+            {
+                return licznik;
+            }
+            set { }
+        }
+
+        private int mianownik;
+        public int Mianownik
+        {
+            get
+            {
+                return mianownik;
+            }
+            set { }
+        }
+
+        //private strin;
+        public Ulamek()
+        {
+            //  this.domyslny = "";
+
+            this.licznik = 0;
+            this.mianownik = 1;
+        }
+
+        public Ulamek (int a, int b)
+        {
+            this.Licznik = a;
+            this.Mianownik = b;
+        }
+
+        public Ulamek (Ulamek c)
+        {
+            this.licznik = c.licznik;
+            this.mianownik = c.mianownik;
+        }
+
+    }
+
 }
